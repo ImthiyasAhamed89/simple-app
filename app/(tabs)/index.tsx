29ad4d1eb9e7383ca57b057bb0b51ef66784e2f6
imthiyas <imthiyas@astasystech.com>
2025-04-25 09:29:25 +0530

    image changed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function App() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/react.jpg')}
           style={styles.reactLogo}
         />
       }>
